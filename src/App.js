b6b80b5102d00526c5803e73b3c60f7264dad058
Victoria-FZ-eng <vzulof@gmail.com>
2021-06-07 13:19:51 +0300

    some styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main'
@@ -7,7 +8,7 @@ import Main from './components/Main'
 class App extends React.Component {
   render(){
     return(
-      <div>
+      <div >
 
         <Header />
         <Main />
--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 class HornedBeasts extends React.Component{
     render(){
         return(
-            <div>
-                <h2>{this.props.title}</h2>
-                <img src={this.props.imgurl} alt="Horned-Beast Photo" />
-                <p>{this.props.desc} </p>
+            <div className='horndiv'>
+                <h2 className='horn'>{this.props.title}</h2>
+                <img className='img'  src={this.props.imgurl} alt="Horned-Beast Photo" />
+                <p className='horn'>{this.props.desc} </p>
             </div>
         )
     }
